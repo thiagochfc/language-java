36d3c82a9531504a132a8e4462e7cb8e90883414
Thiago Christopher <thiago_cfc@hotmail.com>
2018-12-30 14:09:51 -0200

    Renomeando a classe errada e passando Deparment como paramêtro

--- workshop-javafx-jdbc/src/gui/DepartmentFormController.java
+++ workshop-javafx-jdbc/src/gui/DepartmentFormController.java
@@ -8,8 +8,11 @@ import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import model.entities.Department;
 
-public class DepartmentFomrController implements Initializable {
+public class DepartmentFormController implements Initializable {
+    
+    private Department entity;
     
     @FXML
     private TextField txtId;
@@ -25,6 +28,10 @@ public class DepartmentFomrController implements Initializable {
     
     @FXML
     private Button BtCancel;
+    
+    public void setDepartment(Department entity) {
+        this.entity = entity;
+    }
     
     @FXML
     public void onBtSaveAction() {
@@ -44,6 +51,14 @@ public class DepartmentFomrController implements Initializable {
     private void initializeNodes() {
         Constraints.setTextFieldInteger(txtId);
         Constraints.setTextFieldMaxLength(txtName, 30);
+    }
+    
+    public void updateFormData() {
+        if(entity == null) {
+            throw new IllegalStateException("Entitiy was null!");
+        }
+        txtId.setText(String.valueOf(entity.getId()));
+        txtName.setText(entity.getName());
     }
     
 }
