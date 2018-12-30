e496b3bf26c0a5956ed356b6a71d4b80a53a74f6
Thiago Christopher <thiago_cfc@hotmail.com>
2018-12-30 14:42:56 -0200

    Criando a Interface Listeners e implementando na classes

--- workshop-javafx-jdbc/src/gui/DepartmentFormController.java
+++ workshop-javafx-jdbc/src/gui/DepartmentFormController.java
@@ -1,10 +1,13 @@
 package gui;
 
 import db.DbException;
+import gui.listeners.DataChangeListener;
 import gui.util.Alerts;
 import gui.util.Constraints;
 import gui.util.Utils;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
@@ -20,6 +23,7 @@ public class DepartmentFormController implements Initializable {
 
     private Department entity;
     private DepartmentService service;
+    private List<DataChangeListener> dataChangeListeners = new ArrayList<>();
 
     @FXML
     private TextField txtId;
@@ -43,6 +47,10 @@ public class DepartmentFormController implements Initializable {
     public void setDepartmentService(DepartmentService service) {
         this.service = service;
     }
+    
+    public void subscribeDataChangeListener(DataChangeListener listener) {
+        dataChangeListeners.add(listener);
+    }
 
     @FXML
     public void onBtSaveAction(ActionEvent event) {
@@ -55,6 +63,7 @@ public class DepartmentFormController implements Initializable {
         try {
             entity = getFormData();
             service.saveOrUpdate(entity);
+            notifyDataChangeListeners();
             Utils.currentStage(event).close();
         } catch(DbException e) {
             Alerts.showAlert("Error saving object", null, e.getMessage(), AlertType.ERROR);
@@ -89,6 +98,11 @@ public class DepartmentFormController implements Initializable {
         obj.setId(Utils.tryParseToInt(txtId.getText()));
         obj.setName(txtName.getText());
         return obj;
+    }
+
+    private void notifyDataChangeListeners() {
+        for(DataChangeListener listener : dataChangeListeners)
+            listener.onDataChanged();
     }
 
 }
