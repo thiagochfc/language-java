3d19033635e2c6d7e87f8b417f404ceb21b9b186
Thiago Christopher <thiago_cfc@hotmail.com>
2018-12-29 22:48:47 -0200

    Implementando o método de update

--- demo-dao-jdbc/src/model/dao/impl/DepartmentDaoJDBC.java
+++ demo-dao-jdbc/src/model/dao/impl/DepartmentDaoJDBC.java
@@ -54,7 +54,23 @@ public class DepartmentDaoJDBC implements DepartmentDao {
 
     @Override
     public void update(Department obj) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        PreparedStatement st = null;
+        try {
+            st = conn.prepareStatement(
+                "UPDATE department " 
+              + "SET Name = ? "
+              + "WHERE Id = ?"
+            );
+            
+            st.setString(1, obj.getName());
+            st.setInt(2, obj.getId());
+            
+            st.executeUpdate();
+        } catch(SQLException e) {
+            throw new DbException(e.getMessage());
+        } finally {
+            DB.closeStatement(st);
+        }
     }
 
     @Override
