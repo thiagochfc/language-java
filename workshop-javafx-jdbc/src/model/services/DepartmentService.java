376a0507e61f5532dcb2bc0b2614658d11dd4e9e
Thiago Christopher <thiago_cfc@hotmail.com>
2018-12-30 13:12:12 -0200

    Adicionando os arquivos para conexão com banco de dados

--- workshop-javafx-jdbc/src/model/services/DepartmentService.java
+++ workshop-javafx-jdbc/src/model/services/DepartmentService.java
@@ -1,7 +1,8 @@
 package model.services;
 
-import java.util.ArrayList;
 import java.util.List;
+import model.dao.DaoFactory;
+import model.dao.DepartmentDao;
 import model.entities.Department;
 
 /*******************************************************************************
@@ -20,12 +21,10 @@ import model.entities.Department;
 
 public class DepartmentService {
     
+    private DepartmentDao dao = DaoFactory.createDepartmentDao();
+    
     public List<Department> findAll() {
-        List<Department> list = new ArrayList<>();
-        list.add(new Department(1, "Books"));
-        list.add(new Department(2, "Computers"));
-        list.add(new Department(3, "Electronics"));
-        return list;
+        return dao.findAll();
     }
     
 }
