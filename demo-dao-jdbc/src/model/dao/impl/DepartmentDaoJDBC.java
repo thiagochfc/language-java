0916c6be90c58a25519afbdad1a3ec99befb4636
Thiago Christopher <thiago_cfc@hotmail.com>
2018-12-29 22:12:27 -0200

    Implementando o FindAll

--- demo-dao-jdbc/src/model/dao/impl/DepartmentDaoJDBC.java
+++ demo-dao-jdbc/src/model/dao/impl/DepartmentDaoJDBC.java
@@ -1,6 +1,12 @@
 package model.dao.impl;
 
+import db.DB;
+import db.DbException;
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 import model.dao.DepartmentDao;
 import model.entities.Department;
@@ -35,7 +41,26 @@ public class DepartmentDaoJDBC implements DepartmentDao {
 
     @Override
     public List<Department> findAll() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        PreparedStatement st = null;
+        ResultSet rs = null;
+        try {
+            st = conn.prepareStatement(
+                "SELECT * FROM coursejdbc.department"
+            );
+            
+            rs = st.executeQuery();
+            List<Department> list = new ArrayList<>();
+            
+            while(rs.next()) {
+                list.add(new Department(rs.getInt("Id"), rs.getString("Name")));
+            }
+            return list;
+        } catch(SQLException e) {
+            throw new DbException(e.getMessage());
+        } finally {
+            DB.closeStatement(st);
+            DB.closeResultSet(rs);
+        }
     }
     
 }
