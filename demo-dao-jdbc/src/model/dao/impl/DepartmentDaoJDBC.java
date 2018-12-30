298d7028be0938d3ee654b5881d761823d558636
Thiago Christopher <thiago_cfc@hotmail.com>
2018-12-29 22:40:02 -0200

    Implementando o método de inserir

--- demo-dao-jdbc/src/model/dao/impl/DepartmentDaoJDBC.java
+++ demo-dao-jdbc/src/model/dao/impl/DepartmentDaoJDBC.java
@@ -6,6 +6,7 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
 import model.dao.DepartmentDao;
@@ -21,7 +22,34 @@ public class DepartmentDaoJDBC implements DepartmentDao {
     
     @Override
     public void insert(Department obj) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        PreparedStatement st = null;
+        try {
+            st = conn.prepareStatement(
+                "INSERT INTO department "
+              + "(Name) "
+              + "VALUES "
+              + "(?)"
+              , Statement.RETURN_GENERATED_KEYS
+            );
+            
+            st.setString(1, obj.getName());
+            
+            int rowsAffected = st.executeUpdate();
+            if(rowsAffected > 0) {
+                ResultSet rs = st.getGeneratedKeys();
+                if(rs.next()) {
+                    int id = rs.getInt(1);
+                    obj.setId(id);
+                } 
+                DB.closeResultSet(rs);
+            } else {
+                    throw new DbException("Unexpected error! No rows affected.");
+            }
+        } catch(SQLException e) {
+            throw new DbException(e.getMessage());
+        } finally {
+            DB.closeStatement(st);
+        }
     }
 
     @Override
